466bf7dc591fffce97a98032ce86b67b6b30a5f8
Ashraf Hatem <ashraf.omaara47@gmail.com>
2023-08-28 16:41:34 +0300

    Initial commit

---
+++ bin/st_task_1.dart
@@ -0,0 +1,30 @@
+
+void main() {
+
+  //question 1
+  String name = "ashraf";
+  String address = "October";
+  int age = 22;
+  String phone = "01023014472";
+  String email = "ashraf22@gmail.com";
+
+  print("Hello, my name is $name, I live in $address, I am $age years old, this is my phone number $phone, and this is my email $email");
+
+
+  print("--------------------------------------------------");
+
+  // question 2
+  double height = 33.5;
+  double base = 25.25;
+
+  double area = 0.5 * height * base ;
+  print("the area of triangle is : $area");
+
+
+}
+
+
+
+
+
+
---
+++ test/st_task_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:st_task_1/st_task_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
